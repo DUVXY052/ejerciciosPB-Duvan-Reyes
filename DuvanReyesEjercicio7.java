8bc2e1106f03989f7745a9a1eb74d8517fd54075
Duvan Reyes <dreyesxd05@gmail.com>
2024-02-15 19:00:20 -0500

    7

---
+++ DuvanReyesEjercicio7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class DuvanReyesEjercicio7{
+    public static void main (String arg[]){
+        Scanner inter=new Scanner(System.in);
+
+        System.out.println("Ingrese un numero: ");
+
+        int n1=inter.nextInt();
+
+        if (n1 > 0) {
+            System.out.println("El numero es positivo");
+            
+        }else
+            System.out.println("El numero es negativo");
+        
+    }
+}    
