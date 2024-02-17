30059a4eedcf0423bb7aeafb679861e0ec66d48a
Duvan Reyes <dreyesxd05@gmail.com>
2024-02-17 18:42:53 -0500

    EJERCICIO 9

---
+++ DuvanReyesEjercicio9.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class DuvanReyesEjercicio9 {
+
+    public static void main(String[] abc) {
+        Scanner inter=new Scanner(System.in);
+
+        System.out.println("Ingrese la altura del ttriangulo: ");
+
+        float ALTR=inter.nextFloat();
+
+        System.out.println("Ingrese la base del triangulo: ");
+
+        float BAST=inter.nextFloat();
+
+        Float areaT= (ALTR * BAST) / 2;
+
+        System.out.println("El area del triangulo es de " + areaT + " CM");
+    }
+}
