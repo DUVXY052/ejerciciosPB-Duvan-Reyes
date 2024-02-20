0341b390fee66967551319618ee6fcd7e3392c59
Duvan Reyes <dreyesxd05@gmail.com>
2024-02-20 11:34:23 -0500

    2

--- DuvanReyesEjercicio14.java
+++ DuvanReyesEjercicio14.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class DuvanReyesEjercicio14 {
+   public static void main(String[] args){
+      Scanner inter=new Scanner(System.in);
 
-     public static void main(String[] args) {
-        Scanner inter=new Scanner(System.in);
-        
-        System.out.println("Ingrese un numero...");
+      System.out.println("Ingrese un numero de 4 digitos...");
+ 
+      int numb=inter.nextInt();
 
-        int numb=inter.nextInt();
+      int dg1 = numb / 1000;
+      int dg2 = (numb / 100) % 10;
+      int dg3 = (numb / 10) % 10;
+      int dg4 = numb % 10;
 
-        System.out.println("El numero es :" +numb);
-
-        int sumn= 2 + 0 + 0 + 4 
-
-        System.out.println("La suma de sus digitos es igual a " + sumn);
-     }
+      System.out.println("La suma de los dígitos de " + numb + " es " + (dg1 + dg2 + dg3 + dg4));
+   } 
 }
